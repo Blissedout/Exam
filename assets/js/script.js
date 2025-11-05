cf550c0743d6fc7bb1619130d1a8291a0bf1fb70
Blessing <udegbunablessing@gmail.com>
2025-11-05 13:56:10 +0100

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+const numbers = [1, 2, 34, 99, 12, 4, 5, 53];
+
+const largest = Math.max(...numbers);
+const smallest = Math.min(...numbers);
+
+console.log("Largest number:", largest);
+console.log("Smallest number:", smallest);
